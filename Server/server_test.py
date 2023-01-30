d6912cdade3dfbdbd5f1ec8ecced8f059c6294d5
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-01-30 19:43:28 +0100

    probar en la rasp

--- Server/server_test.py
+++ Server/server_test.py
@@ -26,14 +26,12 @@ filename = ip_raspberry[3] + "_" + filename
 filename = os.path.basename(filename)
 filesize = int(filesize)
 
-#progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(filename, "wb") as f:
     while True:
         bytes_read = client_socket.recv(BUFFER_SIZE)
         if not bytes_read:    
             break
         f.write(bytes_read)
-        #progress.update(len(bytes_read))
 
 client_socket.close()
 s.close()
