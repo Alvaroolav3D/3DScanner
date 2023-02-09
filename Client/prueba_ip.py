e30ee436a222bb9053af6a63b813ce34537d3f07
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-09 13:58:40 +0100

    Update prueba_ip.py

--- Client/prueba_ip.py
+++ Client/prueba_ip.py
@@ -1,27 +1,26 @@
 import subprocess
 import re
 
-def get_network_config(interface):
-    output = subprocess.run(["ifconfig", interface], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-    ip_address = re.search("inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output.stdout).group(1)
-    netmask = re.search("netmask ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output.stdout).group(1)
-    gateway = re.search("default gw ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output.stdout).group(1)
-    dns_output = subprocess.run(["cat", "/etc/resolv.conf"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-    dns = re.search("nameserver ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", dns_output.stdout).group(1)
-    return ip_address, netmask, gateway, dns
-
 def get_interface_name():
-    output = subprocess.run(["ifconfig"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-    match = re.search("eth0", output.stdout)
-    if match:
-        return "eth0"
-    else:
-        raise Exception("eth0 interface not found")
+    output = subprocess.check_output(["ifconfig"])
+    output = output.decode("utf-8")
+    interfaces = re.findall("^(\w+)\s", output, re.MULTILINE)
+    return interfaces[0]
 
-if __name__ == "__main__":
-    interface = get_interface_name()
-    ip_address, netmask, gateway, dns = get_network_config(interface)
-    print("IP Address:", ip_address)
-    print("Netmask:", netmask)
-    print("Gateway:", gateway)
-    print("DNS:", dns)
+def get_network_config(interface):
+    output = subprocess.check_output(["ifconfig", interface])
+    output = output.decode("utf-8")
+    ip_address = re.search("inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output).group(1)
+    netmask = re.search("netmask ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output).group(1)
+    gateway = re.search("default gw ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output).group(1)
+    dns = re.search("nameserver ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", output).group(1)
+    return (ip_address, netmask, gateway, dns)
+
+interface = get_interface_name()
+ip_address, netmask, gateway, dns = get_network_config(interface)
+
+print("Interface:", interface)
+print("IP address:", ip_address)
+print("Netmask:", netmask)
+print("Gateway:", gateway)
+print("DNS:", dns)
