d5d866358d758bfb84dc9245603c6c15bb508878
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-10 20:07:13 +0100

    s

--- Client/start.sh
+++ Client/start.sh
@@ -3,4 +3,4 @@
 # Each script is executed in the order specified, and the next script is only 
 # started after the previous one has completed
 python3 /home/pi/Desktop/gitrepo.py
-#python3 /Desktop/3DScanner/Client/client_listen.py
+python3 /home/pi/Desktop/3DScanner/Client/client_listen.py
