d6b5bc8538bbc6e7799fb48e4ad2dbdd21a2a57d
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-10 19:32:44 +0100

    Create start.sh

---
+++ Client/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Each script is executed in the order specified, and the next script is only 
+# started after the previous one has completed
+python3 /Desktop/gitrepo.py
+python3 /Desktop/3DScanner/Client/client_listen.py
