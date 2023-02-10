a5214377aa975d0014d52056b9f1c3e54f17e4a6
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-10 20:23:45 +0100

    update machine

--- ToDesktop/start.sh
+++ ToDesktop/start.sh
@@ -2,8 +2,6 @@
 
 # Each script is executed in the order specified, and the next script is only 
 # started after the previous one has completed
-python3 /home/pi/Desktop/gitrepo.py
-
-sudo chmod +x /home/pi/Desktop/3DScanner/Client/start.sh
+python3 /home/pi/Desktop/git_Update_Repository.py
 
 python3 /home/pi/Desktop/3DScanner/Client/client_listen.py
