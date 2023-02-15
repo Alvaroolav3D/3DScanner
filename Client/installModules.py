5614c22e51f91af20574516e78a3b404c07f4967
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-15 11:50:24 +0100

    Update installModules.py

--- Client/installModules.py
+++ Client/installModules.py
@@ -6,14 +6,12 @@ subprocess.run(['sudo', 'pip3', 'install', '--upgrade', 'pip'])
 
 # Lista de módulos a instalar
 modules = ['subprocess', 'importlib', 'time', 'os', 'datetime', 'socket', 'struct', 'picamera']
-#modules = ['importlib', 'datetime', 'picamera', 'ntplib']
+#los que no vienen dados por el sistema son: ['importlib', 'datetime', 'picamera', 'ntplib']
 
-# Iterar sobre la lista de módulos y comprobar si están instalados
 for module in modules:
     try:
         importlib.import_module(module)
-        print(module + ' ya esta instalado')
+        print(module + ' library is already installed')
     except ImportError:
-        print(module + ' no está instalado, se procedera a instalar')
-        # Utilizar subprocess para ejecutar el comando pip3 install
+        print(module + ' library not found. Installing...')
         subprocess.run(['sudo', 'pip3', 'install', module])
