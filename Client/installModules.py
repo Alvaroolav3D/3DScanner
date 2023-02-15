a8e8fa693f474596ae27f9ee8f1df29b1da4901d
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-15 11:23:18 +0100

    Update installModules.py

--- Client/installModules.py
+++ Client/installModules.py
@@ -11,8 +11,8 @@ modules = ['subprocess', 'importlib', 'time', 'os', 'datetime', 'socket', 'struc
 for module in modules:
     try:
         importlib.import_module(module)
-        print('{} ya está instalado'.format(module))
+        print(module + ' ya está instalado')
     except ImportError:
-        print('{} no está instalado, se procederá a instalar'.format(module))
+        print(module + ' no está instalado, se procederá a instalar')
         # Utilizar subprocess para ejecutar el comando pip3 install
         subprocess.run(['sudo', 'pip3', 'install', module])
