a8442dffa80ed6da30460315efbc66a615907df6
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-02-17 11:29:55 +0100

    ss

---
+++ Server/ips_devices_connected.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+# Check if nmap is installed
+try:
+    subprocess.run(["nmap", "--version"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+except subprocess.CalledProcessError:
+    # If not installed, install nmap
+    print("Installing nmap...")
+    os.system("sudo apt-get update && sudo apt-get install nmap -y")
+
+# Scan for connected devices with Ethernet
+result = subprocess.run(["sudo", "nmap", "-sP", "192.168.1.0/24"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = result.stdout.decode("utf-8")
+
+# Extract IP addresses from the nmap output
+ips = []
+for line in output.split("\n"):
+    if "Nmap scan report for" in line:
+        ip = line.split(" ")[-1]
+        ips.append(ip)
+
+# Print the IP addresses of the connected devices
+print("Connected devices:")
+for ip in ips:
+    print(ip)
