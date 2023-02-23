c9546e34c9a7c40ca359a211d01ec91de2e9513e
Alvaro Olavarria <alvaroolavD2000@gmail.com>
2023-02-23 13:24:11 +0100

    Update ptp_synchronization.py

--- Client/ptp_synchronization.py
+++ Client/ptp_synchronization.py
@@ -4,7 +4,7 @@ server_ip = "192.168.1.200" # IP del servidor
 ptp_interface = "eth0" # Interfaz de red utilizada para PTP
 
 # Comando para ejecutar ptp4l y configurar la sincronización con el servidor
-cmd = ["sudo", "ptp4l", "-i", ptp_interface, "-s", "-f", "/etc/ptp4l.conf", "-m", "-q", "-a", server_ip]
+cmd = ["sudo", "ptp4l", "-i", ptp_interface, "-s", "-f", "/etc/ptp4l.conf", "-m", "-q", server_ip]
 
 # Ejecutar el comando y esperar a que termine
 subprocess.run(cmd)
