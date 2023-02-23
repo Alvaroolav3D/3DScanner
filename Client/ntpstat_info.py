32850cd910a0e31dad486b40d98fcfa90d915e22
Alvaro Olavarria <alvaroolavD2000@gmail.com>
2023-02-23 17:15:25 +0100

    Update ntpstat_info.py

--- Client/ntpstat_info.py
+++ Client/ntpstat_info.py
@@ -2,22 +2,31 @@ import subprocess
 
 def get_ntpstat_info():
     try:
-        output = subprocess.check_output(['ntpstat'], stderr=subprocess.STDOUT)
-        output = output.decode('utf-8').strip()
-        lines = output.split('\n')
+        output = subprocess.check_output(['ntpstat'])
+        lines = output.decode().strip().split('\n')
         status = lines[0].split(': ')[1]
-        if status == 'synchronized':
-            offset = float(lines[1].split(': ')[1])
-            jitter = float(lines[2].split(': ')[1])
-            return {'status': status, 'offset': offset, 'jitter': jitter}
-        else:
-            return {'status': status}
-    except subprocess.CalledProcessError:
-        return {'status': 'error'}
+        info = lines[1:]
+        return status, info
+    except subprocess.CalledProcessError as e:
+        return 'error', [e.output.decode().strip()]
+
+def get_ntp_service_status():
+    try:
+        output = subprocess.check_output(['systemctl', 'status', 'ntp'])
+        lines = output.decode().strip().split('\n')
+        status = lines[2].split(':')[1].strip()
+        return status
+    except subprocess.CalledProcessError as e:
+        return 'error'
 
 ntpstat_info = get_ntpstat_info()
+ntp_service_status = get_ntp_service_status()
 
-if ntpstat_info['status'] == 'synchronized':
-    print('NTP synchronized with an offset of {} ms and a jitter of {} ms'.format(ntpstat_info['offset'], ntpstat_info['jitter']))
+print('NTP service status:', ntp_service_status)
+
+if ntpstat_info[0] == 'synchronised':
+    print('NTP status: synced')
+    print('\n'.join(ntpstat_info[1]))
 else:
-    print('NTP is not synchronized')
+    print('NTP status:', ntpstat_info[0])
+    print('\n'.join(ntpstat_info[1]))
