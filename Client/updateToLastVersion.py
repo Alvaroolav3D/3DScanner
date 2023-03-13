56fc3df7fb744aa52aa9062bcaef5a1a5fca2b96
Alvaroolav3D <alvaroolavD2000@gmail.com>
2023-03-13 11:25:08 +0100

    comit

--- Client/updateToLastVersion.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# Ejecutar el comando "sudo apt-get update"
-subprocess.run(["sudo", "apt-get", "update"])
-
-# Ejecutar el comando "sudo apt-get upgrade"
-subprocess.run(["sudo", "apt-get", "upgrade", "-y"])
