4285afbe74d780e19990cc0cd3d1f6b38f1303b5
Alvaro Olavarria <alvaroolavD2000@gmail.com>
2023-03-13 12:40:14 +0100

    Create pruebas.py

---
+++ Client/pruebas.py
@@ -0,0 +1,21 @@
+import os
+from time import sleep
+
+# lista de nombres de archivo de imagen
+img_files = ['img1.png', 'img2.png']
+
+# tiempo de visualización de cada imagen
+display_time = 1  # segundos
+
+# mostrar cada imagen
+for img_file in img_files:
+    # comprobar si el archivo de imagen existe
+    if not os.path.isfile(img_file):
+        print(f"Error: el archivo {img_file} no existe")
+        continue
+    
+    # mostrar la imagen durante el tiempo especificado
+    os.system(f"sudo fbi -a --noverbose --vt 1 {img_file}")
+    sleep(display_time)
+    
+print("Todas las imágenes han sido mostradas.")
